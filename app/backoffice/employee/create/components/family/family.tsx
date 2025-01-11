bf75e8844c6d404bd7c8661abf59b290af1b4d35
akbarimawan18 <akbarimawan18@gmail.com>
2025-01-11 18:14:50 +0000

    update

--- app/backoffice/employee/create/components/family/family.tsx
+++ app/backoffice/employee/create/components/family/family.tsx
@@ -2,12 +2,23 @@
 "use client";
 
 import { FormInputField } from "@/components/form/field-input";
+import { CirclePlus,Trash } from "lucide-react"
 
 const Family = ({ methods }: { methods: any }) => {
-    return (
-      <div className="grid grid-row-2">
-        <FormInputField control={methods} name="family.name" label="Name" />
-      </div>
+
+  return (
+      <>  
+          <div className="flex gap-2">
+            <div className="grid grid-cols-1 gap-4 w-[90%]">
+              <FormInputField className="custom-field w-full md:w-1/2" form={methods} name="family.nip" label="NIP" />
+            </div>
+            <div className="flex flex-col gap-2">
+               <CirclePlus />
+               <Trash />
+            </div>
+          </div>
+          
+      </>
     );
   };
 
--- app/backoffice/employee/create/components/family/schema.tsx
+++ app/backoffice/employee/create/components/family/schema.tsx
@@ -3,10 +3,8 @@ import { z } from "zod";
 const FamilyValidation = () => {
     return {
         name: z.string().nonempty("Name is required"),
-        ktp: z.string().nonempty("No. KTP is required"),
-        phone: z.string().nonempty("Phone is required"),
-        email: z.string().nonempty("Email is required").email("Invalid email"),
-        place_birth: z.string().nonempty("Place of Birth is required"),
+        relationship: z.string().nonempty("Relationship is required"),
+        education: z.string().nonempty("Education is required"),
         date_birth: z.string().nonempty("Date of Birth is required"),
         gender: z.string().nonempty("Gender is required"),
         religion: z.string().nonempty("Religion is required"),
@@ -14,7 +12,7 @@ const FamilyValidation = () => {
 }
 
 const FamilyDefault = () => {
-    return { name:"", ktp: "", phone:"", email: "", place_birth:"", date_birth: "", gender:"", religion: "" }
+    return { name:"", relationship: "", education:"", date_birth: "", gender:"", religion: "" }
 }
 
 export {FamilyValidation, FamilyDefault}
